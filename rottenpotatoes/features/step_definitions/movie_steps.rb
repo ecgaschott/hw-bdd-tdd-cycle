aa0fdcbaa1d4f81846e50160714f646ec2814e38
Erin Gaschott <ecgaschott@gmail.com>
2015-11-03 02:17:47 +0000

    hit timebox limit, wrote some step defs

---
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,10 @@
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    Movie.create(movie)
+  end
+end
+
+Then /the director of "([^"]*)" should be "([^"]*)"/ do |director, movie|
+    
+end
+
